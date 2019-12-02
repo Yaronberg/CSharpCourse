d24399725abffa35f8efb10f07b75a31588fdc37
Yaronberg <Yaronmen123@gmail.com>
2019-12-02 16:12:53 +0200

    add second exercise

--- PreparationForTheCourseCSharp/№2/Program.cs
+++ PreparationForTheCourseCSharp/№2/Program.cs
@@ -10,6 +10,21 @@ namespace _2
     {
         static void Main(string[] args)
         {
+            string voice = Console.ReadLine();
+            if (voice == "мяу")
+            {
+                Console.WriteLine("Покорми кота, быстро!");
+            }
+            else if (voice == "гав")
+            {
+                Console.WriteLine("Погуляй с собакой, быстро!");
+            }
+            else
+            {
+                Console.WriteLine("Что ты такое?!");
+            }
+            Console.ReadKey();
         }
+
     }
 }
