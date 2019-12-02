9dda13169fe64ef6a51996e891faf03f8b847a86
Yaronberg <Yaronmen123@gmail.com>
2019-12-02 17:10:06 +0200

    add #4 exercise

---
+++ PreparationForTheCourseCSharp/№4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((Console.ReadLine() == "1" ? "Good" : "Bad"));
+
+            Console.ReadKey();
+        }
+    }
+}
