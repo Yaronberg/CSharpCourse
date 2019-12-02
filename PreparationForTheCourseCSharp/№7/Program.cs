b36337011414aeb7459b864e7b34af084b2f2705
Yaronberg <Yaronmen123@gmail.com>
2019-12-02 21:14:53 +0200

    add 7 exercise

---
+++ PreparationForTheCourseCSharp/№7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввидете натуральное число");
+            int a = Int32.Parse(Console.ReadLine());
+            bool result = true;
+            if (a > 1)
+            {
+                for (int i = 2; i < a; i++)
+                {
+                    if (a % i == 0)
+                    {
+                        result = false;
+                        Console.WriteLine("Число не простое");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не натуральное число");
+            }
+
+            if (result)
+            {
+                Console.WriteLine("Число простое");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
