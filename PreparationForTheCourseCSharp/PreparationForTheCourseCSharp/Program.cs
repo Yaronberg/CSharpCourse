2f70b923b9636e9c2b6253e875c323f5ab2b075c
Yaronberg <Yaronmen123@gmail.com>
2019-12-02 15:02:15 +0200

    Create and add first exercise

---
+++ PreparationForTheCourseCSharp/PreparationForTheCourseCSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreparationForTheCourseCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int integer1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int integer2 = Convert.ToInt32(Console.ReadLine());
+            if (integer1 == integer2)
+            {
+                Console.WriteLine("Числа равны");
+            }
+            else if (integer1 > integer2)
+            {
+                Console.WriteLine("Большее число: " + integer1);
+            }
+            else
+            {
+                Console.WriteLine("Большее число: " + integer2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
