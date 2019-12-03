288e1a653f8caee7942892564620bed2034eafc8
Yaronberg <Yaronmen123@gmail.com>
2019-12-03 22:42:25 +0200

    add #14 exercise

---
+++ PreparationForTheCourseCSharp/№14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int rndom = rnd.Next(1, 100);
+                Console.WriteLine($"Число {i + 1}:" + rndom);
+                a += rndom;
+            }
+            Console.WriteLine("Cреднее арифметическое " + a / 5);
+
+            Console.ReadLine();
+        }
+    }
+}
