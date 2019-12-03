e22951a5716f768718d49da23143a6c3d1921cc8
Yaronberg <Yaronmen123@gmail.com>
2019-12-03 22:36:28 +0200

    add #13 exercise

---
+++ PreparationForTheCourseCSharp/№13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Введите число {i+1}:");
+                a += Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("среднее арифметическое " + a / 5);
+
+            Console.ReadLine();
+        }
+    }
+}
