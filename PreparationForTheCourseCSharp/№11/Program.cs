c06ee4d482d015e49512ca8ee0a15130c6bc8c96
Yaronberg <Yaronmen123@gmail.com>
2019-12-03 22:11:07 +0200

    add #11 exercise

---
+++ PreparationForTheCourseCSharp/№11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rand = rnd.Next(1, 147);
+
+            while (true)
+            {
+                Console.WriteLine("Введите число");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                if (a > rand)
+                    Console.WriteLine("Меньше");
+                if (a < rand)
+                    Console.WriteLine("Больше");
+                if (a == rand)
+                {
+                    Console.WriteLine("Вы угадали!");
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
