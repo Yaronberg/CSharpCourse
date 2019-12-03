d8abaae93e9aa7d1ffb3d2cda91899a90e3c5937
Yaronberg <Yaronmen123@gmail.com>
2019-12-03 22:28:59 +0200

    add #12 exercise

---
+++ PreparationForTheCourseCSharp/№12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Просмотр точных квадратов натуральных чисел до числа :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            while (i * i < num)
+            {
+                Console.WriteLine(i * i++);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
