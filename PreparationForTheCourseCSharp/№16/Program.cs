6f73446d3b169da994438a887dea52d51a610df7
Yaronberg <Yaronmen123@gmail.com>
2019-12-03 23:27:38 +0200

    add #16 exercise

---
+++ PreparationForTheCourseCSharp/№16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Введите пароль:");
+                if (Console.ReadLine() == "root")
+                    break;
+                else
+                    Console.WriteLine("Введен неверный пароль");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
