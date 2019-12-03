126dde647db8e64843479ce397d8d8971ae0484d
Yaronberg <Yaronmen123@gmail.com>
2019-12-03 23:16:29 +0200

    add #15 exercise

---
+++ PreparationForTheCourseCSharp/№15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 64; i += 4) 
+                Console.WriteLine($"Гусей {i / 2} Кроликов {(64 - i) /4}");
+            
+            Console.ReadLine();
+        }
+    }
+}
+
