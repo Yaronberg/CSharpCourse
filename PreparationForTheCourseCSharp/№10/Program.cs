86d9df44be02eda7a18c2092b0ff55e1ca06e7f8
Yaronberg <Yaronmen123@gmail.com>
2019-12-03 21:59:32 +0200

    add #10 exercise

---
+++ PreparationForTheCourseCSharp/№10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < 21; i++)
+            {
+                Console.WriteLine($"{a} * {i} = {a * i}");
+            }
+
+            Console.WriteLine("\nВведите число, чтобы узнать количиество разрядов в нем");
+            Console.WriteLine(Console.ReadLine().Length);
+            Console.ReadLine();
+        }
+    }
+}
