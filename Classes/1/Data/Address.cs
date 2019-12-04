ed2a05558d20fd04491a6a68490ab3656c0ecd5e
Yaronberg <Yaronmen123@gmail.com>
2019-12-04 22:45:27 +0200

    Add 1 project

---
+++ Classes/1/Data/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+        class Address
+        {
+            public string Country { get; set; }
+            public string City { get; set; }
+            public string Street { get; set; }
+            public int House { get; set; }
+            public int Apartment { get; set; }
+            public int Index { get; set; }
+
+        }
+}
---
+++ Classes/1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+
+            Console.WriteLine("Введите страну");
+            address.Country = Console.ReadLine();
+            Console.WriteLine("Введите город");
+            address.City = Console.ReadLine();
+            Console.WriteLine("Введите название улицы");
+            address.Street = Console.ReadLine();
+            Console.WriteLine("Введите номер дома");
+            address.House = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите номер квартиры");
+            address.Apartment = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите индекс");
+            address.Index = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Вы ввели такие данные:\n" +
+                            $"Страна - {address.Country}\n" +
+                            $"Город - {address.City}\n" +
+                            $"Улица - {address.Street}\n" +
+                            $"Дом - {address.House}\n" +
+                            $"Квартира - {address.Apartment}\n" +
+                            $"Индекс - {address.Index}\n");
+
+
+            Console.ReadLine();
+        }
+    }
+}
