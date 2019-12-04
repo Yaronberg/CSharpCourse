28635ec29604dabecf9aaf34c82d428a27f383ed
Yaronberg <Yaronmen123@gmail.com>
2019-12-04 23:08:58 +0200

    Add 2 project

---
+++ Classes/2/Data/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Data
+{
+    class Rectangle
+    {
+        double side1 { get; }
+        double side2 { get; }
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+        double PerimeterCalculator()
+        {
+            return side1 + side2;
+        }
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+    }
+}
---
+++ Classes/2/Program.cs
@@ -0,0 +1,21 @@
+﻿using _2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Area - " + rectangle.Area);
+            Console.WriteLine("Perimeter - " + rectangle.Perimeter);
+
+            Console.ReadKey();
+        }
+    }
+}
