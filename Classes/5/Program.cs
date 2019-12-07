8d931479778064c3fec9e8a2a8195bbdb4e4b113
Yaronberg <Yaronmen123@gmail.com>
2019-12-07 03:17:08 +0200

    add tast 5

---
+++ Classes/5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            user.Login = "Aloha";
+            user.Name = "Ivan";
+            user.SurName = "Key";
+            user.Age = 22;
+
+            Console.WriteLine(user.Login + " " + user.Name + " " + user.SurName + " " + user.Age + " " + user.Date);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Classes/5/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        private readonly DateTime date = DateTime.Now;
+        public DateTime Date { get => date; }
+    }
+}
