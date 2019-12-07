fa184c2eff6f13160417e9cb31dbc213795be37d
Yaronberg <Yaronmen123@gmail.com>
2019-12-07 14:57:14 +0200

    add tast 6

---
+++ Classes/6/Converter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6
+{
+    class Converter
+    {
+        public double USD { set; get; }
+        public double EUR { get; set; }
+        public double RUB { get; set; }
+        public Converter(double usd, double eur, double rub)
+        {
+            USD = usd;
+            EUR = eur;
+            RUB = rub;
+        }
+        public void ConvertTo ()
+        {
+            Console.WriteLine("Введите сумму которую хотите конвертировать в другие валюты");
+            double sum = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите валюту которую хотите конвертировать в гривны (USD, EUR, RUB) или наоброт");
+            switch (Console.ReadLine().ToUpper())
+            {
+                case "USD":
+                    Console.WriteLine($"UAH = {sum * USD}");
+                    break;
+                case "EUR":
+                    Console.WriteLine($"UAH = {sum * EUR}");
+                    break;
+                case "RUB":
+                    Console.WriteLine($"UAH = {sum * RUB}");
+                    break;
+                case "UAH":
+                    Console.WriteLine($"{sum}UAH = {sum / USD} USD");
+                    Console.WriteLine($"{sum}UAH = {sum / EUR} EUR");
+                    Console.WriteLine($"{sum}UAH = {sum / RUB} RUB");
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели некорректное значение! Попробуйте еще");
+                    break;
+            }
+        }
+    }
+}
---
+++ Classes/6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(23.70, 26.0, 0.35);
+
+            converter.ConvertTo();
+
+            Console.ReadLine();
+        }
+    }
+}
