406d678e6dcd76501fb16ea5637520996ff1f196
Yaronberg <Yaronmen123@gmail.com>
2019-12-13 22:15:24 +0200

    add 1 task

---
+++ Inheritance/1/BlueColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1
+{
+    class BlueColor : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            base.Print(value);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Inheritance/1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new BlueColor();
+            printer.Print("Синий цвет");
+            printer = new Printer();
+            ((Printer)printer).Print("Базовый цвет");
+
+            Console.WriteLine();
+        }
+    }
+}
