38fea0250530fa1ba9069c22bc7b7c12facb4bce
Yaronberg <Yaronmen123@gmail.com>
2020-01-09 13:32:58 +0200

    1 and 2 task

---
+++ Structure/1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("PSAF44", "ASUS", 700);
+            notebook.Display();
+
+            Console.ReadKey();
+        }
+    }
+    struct Notebook
+    {
+        string model;
+        string brand;
+        double price;
+
+        public Notebook(string model, string brand, double price)
+        {
+            this.model = model;
+            this.brand = brand;
+            this.price = price;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Производитель - {brand}, Модель - {model}, Цена - {price}");
+        }
+    }
+}
---
+++ Structure/2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Train[] trains = new Train[7];
+
+            for (int i = 0; i < trains.Length; i++) // ввод данных 
+            {
+                Console.Write($"\nВведите номер поезда{i}:");
+                int trainNumber = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите пункт назначения:");
+                string destination = Console.ReadLine();
+                Console.Write("Введите время отправления:");
+                string time = Console.ReadLine();
+
+                trains[i] = new Train(trainNumber, destination, time);
+            }
+            
+            Array.Sort(trains, (x, y) => x.trainNumber.CompareTo(y.trainNumber)); //сортировка массива по номеру поезда
+
+            Console.WriteLine("Введите номер поезда:");
+
+            while (true) 
+            {
+                int num = Convert.ToInt32(Console.ReadLine()); //номер поезда введен с клавиатуры 
+                Train q = trains.FirstOrDefault(x => x.trainNumber == num); //поиск структуры по номеру поезда
+                if ((object)q == null) 
+                {
+                    Console.WriteLine("Такого номера поезда нет, попробуйте другой номер");
+                }
+                else
+                {
+                    q.Display();
+                    break;
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+        struct Train
+        {
+            public int trainNumber;
+            public string destination;
+            public string departureTime;
+
+            public Train(int trainNumber, string destination, string departureTime)
+            {
+                this.trainNumber = trainNumber;
+                this.destination = destination;
+                this.departureTime = departureTime;
+            }
+            public void Display()
+            {
+                Console.WriteLine(trainNumber + "\n" + destination + "\n" + departureTime);
+            }
+        }
+    }
+}
