cafa0869f40da9dbd51a79c0f04a439544ea3f81
Yaronberg <Yaronmen123@gmail.com>
2020-01-10 18:07:57 +0200

    task 5 and 6

---
+++ Structure/5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Console.WriteLine("Введите цвет, где 1 = Белый, 2 = Синий, 3 = Красный, 4 = Зеленый ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Painter.Print(str, num);
+
+            Console.ReadLine();
+        }
+    }
+    enum Color
+    {
+        White = 1,
+        Blue,
+        Red,
+        Green
+    }
+    static class Painter
+    {
+
+        public static void Print (string stroka, int color)
+        {
+
+            switch (color)
+            {
+                case (int)Color.White:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case (int)Color.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case (int)Color.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case (int)Color.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(stroka);
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Structure/6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accauntant accauntant = new Accauntant();
+
+            if ( accauntant.AskForBonus(Post.Developer, 180))
+            {
+                Console.WriteLine("Вы заслужили премию!");
+            }
+            else
+            {
+                Console.WriteLine("Надо больше работать!");
+            }
+        }
+    }
+    enum Post
+    {
+        Developer = 160,
+        Manager = 150,
+        Cleaner = 30,
+        TechSupport = 80
+    }
+    class Accauntant
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            if ( (int)worker < hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
