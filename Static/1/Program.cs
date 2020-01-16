b3fa01b09e7f6c981bca569a61620803973d9798
Yaronberg <Yaronmen123@gmail.com>
2020-01-16 19:08:34 +0200

    add task 1 of static

---
+++ Static/1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Calc.Sum(2, 4);
+
+            Console.WriteLine(Calc.history);
+
+            int b = Calc.Multiply(121, 2);
+
+            Console.WriteLine(Calc.history);
+
+
+            Console.ReadLine();
+        }
+    }
+
+    static class Calc
+    {
+        public static string history;
+        public static int Sum(int a, int b) 
+        {
+            int c = a + b;
+            history += $"{a} + {b} = {c}\n";
+            return c;
+        }
+        public static int Subtract(int a, int b)
+        {
+            int c = a - b;
+            history += $"{a} - {b} = {c}\n";
+            return c;
+        }
+        public static int Multiply(int a, int b)
+        {
+            int c = a * b;
+            history += $"{a} * {b} = {c}\n";
+            return c; ;
+        }
+        public static int Divide(int a, int b)
+        {
+            int c = a / b;
+            history += $"{a} / {b} = {c}\n";
+            return c;
+        }
+    }
+}
