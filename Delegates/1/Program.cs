62b2562ad4eb1439cb1cc73e49e9c72472ecc60d
Yaronberg <Yaronmen123@gmail.com>
2020-02-02 07:09:10 +0200

    task 1

---
+++ Delegates/1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            int c = 15;
+
+            MyDelegate myDelegate = (a, b, c) => (a + b + c) / 3;
+            Console.WriteLine(myDelegate(a,b,c));
+
+            Console.ReadLine();
+        }
+
+        delegate double MyDelegate(int a, int b, int c);
+    }
+}
