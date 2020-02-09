065d24dbf4759c4376d190c6e5e0a924f38abe03
Yaronberg <Yaronmen123@gmail.com>
2020-02-09 18:27:53 +0200

    task1

---
+++ Generic/1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = MyClass<int>.FactoryMethod();
+            Console.WriteLine(myClass);
+
+            var myClass1 = MyClass<char>.FactoryMethod();
+            Console.WriteLine(myClass1);
+
+            Console.ReadLine();
+        }
+    }
+    class MyClass<T> where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+}
