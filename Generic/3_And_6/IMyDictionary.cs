cd4c80291ad48bc1ab02f491546b0b8dee5da6d7
Yaronberg <Yaronmen123@gmail.com>
2020-02-10 16:02:37 +0200

    rename 3 task. Add 6task in 3 task

---
+++ Generic/3_And_6/IMyDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3
+{
+    interface IMyDictionary<TKey, TValue>
+    {
+        
+        void Add(TKey k, TValue v);
+        
+        TValue this[TKey i] { get; } 
+        int Count { get;}
+
+    }
+}
